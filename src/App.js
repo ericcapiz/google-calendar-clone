e7d0647b2054e4ec1c029266a689c9e9efffa62a
ericcapiz <ericcapiz@gmail.com>
2021-09-24 14:16:17 -0500

    days of current month render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import React, {useState} from 'react'
+import CalendarHeader from './components/CalendarHeader';
+import Sidebar from './components/Sidebar';
+import Month from './components/Month';
+import {getMonth} from './util'
 import './App.css';
 
 function App() {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+
   return (
-    <div className="App">
-     calendar
-    </div>
+    <>
+      <div className="h-screen flex flex-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/CalendarHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CalendarHeader = () => {
+    return (
+        <div>
+            cal header
+        </div>
+    )
+}
+
+export default CalendarHeader
